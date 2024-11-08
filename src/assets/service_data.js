819d3780c824679ab6f86ab3eb319ba1ebcce563
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-08 08:46:43 -0800

    added images and icons the assets folder

---
+++ src/assets/service_data.js
@@ -0,0 +1,34 @@
+const Services_Data = [
+    {
+        s_no:"01",
+        s_name:"Web design",
+        s_desc:"Creating visually appealing and user-friendly website layouts that align with brand identity and user needs."
+    },
+    {
+        s_no:"02",
+        s_name:"Graphics design",
+        s_desc:"Designing custom graphics, logos, and visual elements to enhance the aesthetic appeal and branding of websites."
+    },
+    {
+        s_no:"03",
+        s_name:"Social media",
+        s_desc:"Developing strategies and designs for incorporating social media platforms into websites, ensuring seamless connectivity and engagement."
+    },
+    {
+        s_no:"04",
+        s_name:"App design",
+        s_desc:"Designing intuitive user interfaces and experiences for mobile and web applications to enhance usability and functionality."
+    },
+    {
+        s_no:"05",
+        s_name:"Performance Optimization",
+        s_desc:"Analyzing and improving website speed and performance, including optimizing images, scripts, and overall loading times to enhance user experience."
+    },
+    {
+        s_no:"06",
+        s_name:"Content writing",
+        s_desc:"Creating written material for digital platforms, aimed at engaging, informing, or entertaining an audience while also optimizing for search engines."
+    },
+]
+ 
+export default Services_Data;
