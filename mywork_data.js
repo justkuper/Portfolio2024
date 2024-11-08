6d685c72c7ab4bceb4094aae398dbb718a8b97a3
justkuper <kbank1988@gmail.com>
2024-11-08 09:03:28 -0800

    Added Asset files
    added images, icons, and data sheets

---
+++ mywork_data.js
@@ -0,0 +1,78 @@
+import project1_img from '../assets/coming_soon.png'
+import project2_img from '../assets/coming_soon.png'
+import project3_img from '../assets/coming_soon.png'
+import project4_img from '../assets/coming_soon.png'
+import project5_img from '../assets/coming_soon.png'
+import project6_img from '../assets/coming_soon.png'
+import project7_img from '../assets/coming_soon.png'
+import project8_img from '../assets/coming_soon.png'
+import project9_img from '../assets/coming_soon.png'
+import project10_img from '../assets/coming_soon.png'
+import project11_img from '../assets/coming_soon.png'
+import project12_img from '../assets/coming_soon.png'
+
+const mywork_data = [
+    {
+        w_no:1,
+        w_name:"Task Manager",
+        w_img:project1_img
+    },
+    {
+        w_no:2,
+        w_name:"Web design",
+        w_img:project2_img
+    },
+    {
+        w_no:3,
+        w_name:"Web design",
+        w_img:project3_img
+    },
+    {
+        w_no:4,
+        w_name:"Web design",
+        w_img:project4_img
+    },
+    {
+        w_no:5,
+        w_name:"Web design",
+        w_img:project5_img
+    },
+    {
+        w_no:6,
+        w_name:"Web design",
+        w_img:project6_img
+    },
+    {
+        w_no:7,
+        w_name:"Web design",
+        w_img:project7_img
+    },
+    {
+        w_no:8,
+        w_name:"Web design",
+        w_img:project8_img
+    },
+    {
+        w_no:9,
+        w_name:"Web design",
+        w_img:project9_img
+    },
+    {
+        w_no:10,
+        w_name:"Web design",
+        w_img:project10_img
+    },
+    {
+        w_no:11,
+        w_name:"Web design",
+        w_img:project11_img
+    },
+    {
+        w_no:12,
+        w_name:"Web design",
+        w_img:project12_img
+    },
+    
+]
+ 
+export default mywork_data;
