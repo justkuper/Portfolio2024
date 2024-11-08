8629e83a6e68ab3e83c92ffbb81ad041e6099e3a
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-08 10:30:33 -0800

    fixed mainjsx to import indexcss

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
   server: {
     port: process.env.PORT || 3000, // Use the environment variable or default to 3000 for local
-    host: '0.0.0.0',                 // Bind to all network interfaces
+    host: true,                 // Bind to all network interfaces
   },
 });
