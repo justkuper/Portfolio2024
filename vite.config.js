49723a0c2262a58bb61d7b3a03d47f191cd14db6
Kuper Bank <KuperBank@Tommi-Roses-15-MacBook-Pro-2.local>
2024-11-08 10:21:33 -0800

    changed viteconfig again

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 3000, // Use the environment variable or default to 3000 for local
+    host: '0.0.0.0',                 // Bind to all network interfaces
+  },
+});
